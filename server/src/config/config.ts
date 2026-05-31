9ea664d68a6e47d1d5ad5287bb994a4e9d4c6121
sobhan661 <sobhantheimpaler@gmail.com>
2026-05-31 15:29:40 +0330

    Update config settings

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({ path: '../../.env' });
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 const config = {
   databaseURL: process.env.DATABASE_URL,
   saltRounds: Number(process.env.SALT_ROUNDS)!,
+  port: process.env.PORT,
 };
 
 export default config;
